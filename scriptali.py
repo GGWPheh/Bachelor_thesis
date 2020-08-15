2ddbe2db8169b5e1b2d8757b28489632b5badcaa
GGWPheh <63057994+GGWPheh@users.noreply.github.com>
2020-08-15 16:35:13 +0200

    Update scriptali.py

--- scriptali.py
+++ scriptali.py
@@ -7,23 +7,29 @@
 # 
 import pymysql
 import sys 
+import os
 
-C=pymysql.connect(host="localhost",user="root",db="test",password="yy")
+C=pymysql.connect(host="localhost",user="root",db="SNP",password="yy")
 c=C.cursor()
 
 keyword=sys.argv[1]
+keyfile = "/tmp/%s_window.txt"%keyword
+cmd="""select snpid,window from %s""" % (keyword)
+alreadydone=0
 
-cmd="""select snpid,window from data limit 3"""
-c.execute(cmd)
-var=c.fetchall()
-# ~ print (var)
-dwin=open("./%s_window.txt","w")%keyword
-for i in var :
-	vor=list(i)
-	dwin.write(">")
-	dwin.write(vor[0])
-	dwin.write("\n")
-	dwin.write(vor[1])
-	dwin.write("\n")
-dwin.close()
+if not os.path.isfile(keyfile):
+	c.execute(cmd)
+	var=c.fetchall()
+	# ~ print (var)
+	dwin=open("/tmp/%s_window.txt"%keyword,"w")
+	for i in var :
+		vor=list(i)
+		dwin.write(">")
+		dwin.write(vor[0])
+		dwin.write("\n")
+		dwin.write(vor[1])
+		dwin.write("\n")
+	dwin.close()
+else :
+	alreadydone +=1
 
