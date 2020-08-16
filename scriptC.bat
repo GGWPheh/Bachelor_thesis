0abdf9c286c84c86af6b1b2d2880d10a6d1ab52c
Zehams <60796527+Zehams@users.noreply.github.com>
2020-08-17 00:09:18 +0200

    Update scriptC.bat

--- scriptC.bat
+++ scriptC.bat
@@ -1,2 +1,9 @@
-python ./snpstep1.py $1
-python ./scriptxml.py $1
+python ./step0.py $1 $2
+if [ -f /tmp/flag ]
+then
+	python ./snpstep1.py $1
+	python ./scriptxml.py
+	rm /tmp/flag
+else
+	echo "This Database already Exist !!"
+fi
