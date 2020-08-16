a85b137d6f5ee5f4ad6cd49a6b2d740638373db6
Zehams <60796527+Zehams@users.noreply.github.com>
2020-08-17 00:07:40 +0200

    Update scriptA.bat

--- scriptA.bat
+++ scriptA.bat
@@ -1,11 +1,13 @@
-python ./step0.py $1
-if [ -f flag ]
+python ./step0.py $1 $2
+if [ -f /tmp/flag ]
 then
 	python ./step1.py $1
 	python ./split.py
 	make -j4
 	python ./resume.py
 	python ./step3.py $1
+	rm /tmp/flag
 else
 	echo "This Database already Exist !!"
+	rm /tmp/flag
 fi
