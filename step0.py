fc1fe5e920ec7006544db0f2b3056ae730f42526
GGWPheh <63057994+GGWPheh@users.noreply.github.com>
2020-08-17 00:07:35 +0200

    Update step0.py

--- step0.py
+++ step0.py
@@ -3,8 +3,68 @@
 print("Content-type:text/html\n")
 print
 
-
 import sys
+import os
+import entrezpy.conduit
+
+keyword = sys.argv[1]
+prog = sys.argv[2]
+# ~ keyfile = "/tmp/%s.txt"%keyword
+
+if prog == "SNP" :
+	C=pymysql.connect(host="localhost",user="root",db="SNP",password="yy")
+	c=C.cursor()
+	
+	cmdverif="""show tables;"""
+	c.execute(cmdverif)
+	res=c.fetchall()
+	verif_table=[]
+	for a in res :
+		for b in range(len(a)):
+			verif_table.append(a[b])
+	
+	if keyword in verif_table :
+		print ("database already exist")
+	
+	else :
+		 w = entrezpy.conduit.Conduit('email')
+		get_doc = w.new_pipeline() 
+		sid = get_doc.add_search({'db' : 'pubmed', 'term' : keyword , 'rettype' : 'count'})
+		get_doc.add_fetch({'db' : 'pubmed','retmode' : 'text', 'rettype' : 'medline'}, dependency=sid)	
+		print (" PUBMED : %s  !! TELECHAREMENT EN COURS !! "%keyword)
+		sys.stdout = open(keyfile, 'w')
+		analyzer = w.run(get_doc)
+
+else :
+	C=pymysql.connect(host="localhost",user="root",db="TFE",password="yy")
+	c=C.cursor()
+	
+	cmdverif="""show tables;"""
+	c.execute(cmdverif)
+	res=c.fetchall()
+	verif_table=[]
+	for a in res :
+		for b in range(len(a)):
+			verif_table.append(a[b])
+	
+	if keyword in verif_table :
+		print ("database already exist")
+	
+	else :
+		 w = entrezpy.conduit.Conduit('email')
+		get_doc = w.new_pipeline() 
+		sid = get_doc.add_search({'db' : 'pubmed', 'term' : keyword , 'rettype' : 'count'})
+		get_doc.add_fetch({'db' : 'pubmed','retmode' : 'text', 'rettype' : 'medline'}, dependency=sid)	
+		print (" PUBMED : %s  !! TELECHAREMENT EN COURS !! "%keyword)
+		sys.stdout = open(keyfile, 'w')
+		analyzer = w.run(get_doc)
+
+
+
+
+
+
+"""import sys
 import os
 import entrezpy.conduit
 
@@ -21,4 +81,4 @@ if not os.path.isfile(keyfile):
 	sys.stdout = open(keyfile, 'w')
 	analyzer = w.run(get_doc)
 else:
-	print("Database Found")
+	print("Database Found")"""
