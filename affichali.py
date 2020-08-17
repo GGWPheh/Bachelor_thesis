64d8c7fec4af5008e213d6f3889533fda8452c40
Zehams <60796527+Zehams@users.noreply.github.com>
2020-08-17 17:58:55 +0200

    Update affichali.py

--- affichali.py
+++ affichali.py
@@ -1,28 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-#  refreq.py
-#  
-#  Copyright 2020 root <root@Laptop Zehams & Kydae>
-# 
-import pymysql
+print("Content-type:text/html\n")
+print()
+
+print("<pre>")
+
 import sys 
-import os
 
 keyword=sys.argv[1]
-keyfile = "/tmp/%s_res.vcf"%keyword
-alreadydone=0
 
-if not os.path.isfile(keyfile):
-	dwin=open("/tmp/%s.vcf"%keyword,"r")
-	text=dwin.read()
-	dwin.close()
-	df=open("/tmp/%s_res.vcf"%keyword,"w")
-	df.write("Content-type:text/html")
-	df.write("\n")
-	df.write("<pre>")
-	df.write("\n")
-	df.write(text)
-	df.close()
-else :
-	alreadydone +=1
+dwin=open("/tmp/%s.vcf"%keyword,"r")
+text=dwin.readlines()
+verif=0
+for l in (text):
+	if verif:print(l)
+	if ("#CHROM") in l:
+		print(l)
+		verif = 1
